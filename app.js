c9c2a424a800ec04eebc930c3c68981a42ecd067
Dominik Leitner <leitner.dominik@hotmail.de>
2019-07-27 22:07:23 +0200

    add headers and error-middleware

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 const MONGODB_URI =
-  'mongodb+srv://dominik:TbUzGgf4LLRi06IE@cluster0-dzuqr.mongodb.net/instapic';
+  'mongodb+srv://dominik:TbUzGgf4LLRi06IE@cluster0-dzuqr.mongodb.net/notes';
 
 app.use(bodyParser.json());
 
-// app.use('/auth', authRoutes);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.use('/auth', authRoutes);
 // app.use('/notes', notesRoutes);
+
+app.use((error, req, res, next) => {
+  // console.log('error: ', error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message, data });
+});
 
 mongoose
   .connect(MONGODB_URI)
