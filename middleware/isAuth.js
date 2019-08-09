4df66c24358672852f9779e3aa054cbacbf8bc1b
Dominik Leitner <bigtoasti@gmail.com>
2019-08-09 21:30:23 +0200

    add authentication middleware

---
+++ middleware/isAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, 'supersecretpassword');
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
